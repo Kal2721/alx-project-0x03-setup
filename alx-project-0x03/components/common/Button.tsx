8c5e055f5ba62a7da8283d35a3b1d1ec76216b3a
kalkidan <kalkidanteshale6@gmail.com>
2025-07-27 09:18:42 +0300

    Organizing the interfaces with in one directory

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,11 +1,6 @@
-interface ButtonProps {
-	buttonLabel: string
-	buttonSize?: string
-	buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-	action?: () => void
-}
+import { ButtonProps } from "@/interface";
 
-const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
+	const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
 	const backgroundColorClass = buttonBackgroundColor ? {
 		red: 'bg-red-500',
 		blue: 'bg-blue-500',
